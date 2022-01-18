0de95c6e1604f738d014b320fd1a8bc1d4ebbbfb
sadafasad <sadaf98x@gmail.com>
2022-01-18 18:11:31 -0500

    Mandatory variables' and functions' outline added

--- SecretWord.java
+++ SecretWord.java
@@ -3,4 +3,19 @@
 
 public class SecretWord {
 
+    String actual_word;
+    String current_state_of_secret_word;
+
+    public static boolean containsLetter(char letter){
+        return false;
+    }
+
+    public static boolean hasLettersRemaining(){
+        return false;
+    }
+
+    public static String myToString(){
+        return "";
+    }
+
 }
