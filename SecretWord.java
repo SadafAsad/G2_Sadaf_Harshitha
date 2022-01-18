440591cbe730a8f19c6cf9e4874e9b8bd316f3ec
sadafasad <sadaf98x@gmail.com>
2022-01-18 18:14:38 -0500

    numberOfLines() function added

--- SecretWord.java
+++ SecretWord.java
@@ -1,5 +1,9 @@
 //Secret Word Class
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
 
 public class SecretWord {
 
@@ -17,5 +21,28 @@ public class SecretWord {
     public static String myToString(){
         return "";
     }
+
+    public static int numberOfLines(String file_name){
+        
+        // this variable store the number of lines
+        int line_counter = 0;
+
+        // basically i am reading the file and counting the lines
+        try {
+            File file_obj = new File(file_name);
+            Scanner file_reader = new Scanner(file_obj);
+            while (file_reader.hasNextLine()) {
+                file_reader.nextLine();
+                line_counter++;
+            }
+            file_reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return line_counter;
+    }
+
 
 }
