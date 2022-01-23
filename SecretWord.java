ca7bae30b0d000bd36c50d0566ed3e2494c54a4e
Harshitha Sompura <sompharshi123@gmail.com>
2022-01-23 16:55:19 +0530

    Updated Secret Word Class

--- SecretWord.java
+++ SecretWord.java
@@ -1,5 +1,4 @@
 //Secret Word Class
-
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,27 +6,28 @@ import java.util.Random;
 
 public class SecretWord {
 
-    private String actual_word;
-    private String current_state_of_secret_word;
+    private String actualWord;
+    private String currentStateOfSecretWord;
 
-    public SecretWord(String file){
-        setActualWord(wordGenerator(file));
+    public SecretWord(){
+    	
+        setActualWord(wordGenerator());
 
         // for each char in the actual word, we have a _ in the current state
-        int chars_num = this.actual_word.length();
+        int charsNum = this.actualWord.length();
         String str = "";
-        for (int i=0; i<chars_num; i++){
+        for (int i=0; i< charsNum; i++){
             str = str.concat("_");
         }
 
         setCurrentStateOfSecretWord(str);
     }
 
-    public String getActualWord(){ return actual_word; }
-    private void setActualWord(String word){ actual_word = word; }
+    public String getActualWord(){ return actualWord; }
+    private void setActualWord(String word){ actualWord = word; }
 
-    private String getCurrentStateOfSecretWord(){ return current_state_of_secret_word; }
-    private void setCurrentStateOfSecretWord(String current_state){ current_state_of_secret_word = current_state; }
+    public String getCurrentStateOfSecretWord(){ return this.currentStateOfSecretWord; }
+    public void setCurrentStateOfSecretWord(String currentState){ this.currentStateOfSecretWord = currentState; }
 
     public boolean containsLetter(String letter){
         if (getActualWord().contains(letter.toUpperCase())) return true;
@@ -35,70 +35,82 @@ public class SecretWord {
     }
 
     public boolean hasLettersRemaining(){
-        if (getCurrentStateOfSecretWord().contains("_")) return true;
-        return false;
+        if (getCurrentStateOfSecretWord().contains("_")) {
+        	return true;
+        } else {
+        	 return false;
+        }
     }
 
     public String toString(){
-        int word_len = getCurrentStateOfSecretWord().length();
-        String current_state_displayer = "";
+        int wordLen = getCurrentStateOfSecretWord().length();
+        String currentStateDisplayer = "";
 
         // adds " " after each character
-        for(int i=0; i<word_len; i++){
-            current_state_displayer = current_state_displayer + getCurrentStateOfSecretWord().substring(i, i+1) + " ";
+        for(int i=0; i< wordLen; i++){
+        	currentStateDisplayer = currentStateDisplayer + getCurrentStateOfSecretWord().substring(i, i+1) + " ";
         }
-        return current_state_displayer;
+        return currentStateDisplayer;
     }
 
-    private int numberOfLines(String file_name){
+    private int numberOfLines(){
         
         // this variable store the number of lines
-        int line_counter = 0;
+        int lineCounter = 0;
 
         // basically it's reading the file and counting the lines
         try {
-            File file_obj = new File(file_name);
-            Scanner file_reader = new Scanner(file_obj);
-            while (file_reader.hasNextLine()) {
-                file_reader.nextLine();
-                line_counter++;
+            //File fileObj = new File(fileName);
+            File starting = new File(System.getProperty("user.dir"));
+    		File fileToBeRead = new File(starting,"words.txt");
+            
+    		Scanner fileReader = new Scanner(fileToBeRead);
+            while (fileReader.hasNextLine()) {
+                fileReader.nextLine();
+                lineCounter++;
             }
-            file_reader.close();
+            fileReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
-        return line_counter;
+        return lineCounter;
     }
 
-    private String wordGenerator(String file_name){
+    private String wordGenerator(){
+    	
 
         // this variable stores the word which is to be guessed
-        String secret_word = "";
-        int number_of_lines_in_file = numberOfLines(file_name);
-
+        String secretWord = "";
+        int numberOfLinesInFile = numberOfLines();
+        
         // a random number is being generated
         // which is used to read a random line from the file
         Random rand = new Random();
-        int random_int = rand.nextInt(number_of_lines_in_file+1);
+        int randomInt = rand.nextInt(numberOfLinesInFile +1);
 
         // it's reading the file untill it gets to the random line to be read
         try {
-            File file_obj = new File(file_name);
-            Scanner file_reader = new Scanner(file_obj);
-            for (int i=0; i<random_int; i++) {
-              file_reader.nextLine();
+        	File starting = new File(System.getProperty("user.dir"));
+    		File fileToBeRead = new File(starting,"words.txt");
+            
+    		Scanner fileReader = new Scanner(fileToBeRead);
+          
+            for (int i=0; i<randomInt; i++) {
+              fileReader.nextLine();
             }
-            secret_word = file_reader.nextLine();
-            file_reader.close();
+            secretWord = fileReader.nextLine();
+            fileReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
-        return secret_word.toUpperCase();
+        return secretWord.toUpperCase();
     }
 
 
 }
+
+
