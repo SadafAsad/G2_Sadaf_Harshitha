4726d22ee20ec006ff594bd1796e831131598eea
sadafasad <sadaf98x@gmail.com>
2022-01-23 13:33:22 -0500

    unnecessary variable deleted

--- PreviousGameResult.java
+++ PreviousGameResult.java
@@ -1,7 +1,6 @@
 // Previous Game Result class
 
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -10,12 +9,8 @@ import java.time.LocalDateTime;
 
 public class PreviousGameResult {
 
-    //private DateTimeFormatter date_time;
     private String datePlayed;
     private int points = 0;
-
-    private ArrayList<String> resultsOfGames = new  ArrayList<String>(); 
-    
     
     public PreviousGameResult(int points){
     	setDatePlayed();
@@ -32,11 +27,6 @@ public class PreviousGameResult {
 
     public int getPoints(){ return points; }
     public void setPoints(int points){ this.points = points; }
-    
-    public void convertResultsIntoList(String datePlayed, int points) {
-    	this.resultsOfGames.add("Date/Time: " + this.datePlayed + "\n" + "Score: " + getPoints() + "\n");  
-    }
-    
     
     public String toString(){
     	return "Date/Time: " + this.datePlayed + "\n" + "Score: " + getPoints() + "\n";
